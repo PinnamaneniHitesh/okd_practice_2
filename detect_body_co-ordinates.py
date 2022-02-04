60f7559df85728ec39f2dd378d886ba62d8ecb99
siddu1717 <94636697+siddu1717@users.noreply.github.com>
2022-02-04 18:48:41 +0530

    Create detect_body_co-ordinates.py

---
+++ detect_body_co-ordinates.py
@@ -0,0 +1,33 @@
+import cv2
+
+import mediapipe as mp
+from mediapipe.framework.formats import detection_pb2
+
+cap = cv2.VideoCapture(0)
+
+mpHolistic = mp.solutions.holistic
+holistic = mpHolistic.Holistic()
+mpDraw = mp.solutions.drawing_utils
+drawing_specs = mpDraw.DrawingSpec(thickness=1, circle_radius=1)
+
+while True:
+    success,img = cap.read()
+    if not success:
+        break
+        
+    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results = holistic.process(imgRGB)
+    
+    mpDraw.draw_landmarks(img,results.face_landmarks,mpHolistic.FACEMESH_CONTOURS,drawing_specs,drawing_specs)
+    mpDraw.draw_landmarks(img,results.right_hand_landmarks,mpHolistic.HAND_CONNECTIONS,drawing_specs,drawing_specs)
+    mpDraw.draw_landmarks(img,results.left_hand_landmarks,mpHolistic.HAND_CONNECTIONS,drawing_specs,drawing_specs)
+    mpDraw.draw_landmarks(img,results.pose_landmarks,mpHolistic.POSE_CONNECTIONS,drawing_specs,drawing_specs)
+    
+    cv2.imshow('image',img)
+    
+    if cv2.waitKey(1) & 0xff ==ord('q'):
+        break
+        
+cap.release()
+cap.DestroyAllWindows()
+
